2bdafe0f28cb01030005c633ae3bf15fee83ad38
calebthewood <calebwood.cs@gmail.com>
2022-07-23 13:05:01 -0700

    Add routeRobot and goalOrientedRobot

--- Ch7-Robot/robot.js
+++ Ch7-Robot/robot.js
@@ -10,8 +10,18 @@ const roads = [
   "Marketplace-Town Hall", "Shop-Town Hall"
 ];
 
+const mailRoute = [
+  "Alice's House", "Cabin", "Alice's House", "Bob's House",
+  "Town Hall", "Daria's House", "Ernie's House",
+  "Grete's House", "Shop", "Grete's House", "Farm",
+  "Marketplace", "Post Office"
+];
+
 /**
- *Converts an array of "to-from" elements to a graph
+ *Converts an array of "to-from" elements to a graph.
+
+ * Note: this graph is connected, meaning that every
+ * location can be reached from all other locations.
  */
 function buildGraph(edges) {
   let graph = Object.create(null);
@@ -105,7 +115,7 @@ function randomRobot(state) {
 }
 
 
-VillageState.random = function(parcelCount = 5) {
+VillageState.random = function (parcelCount = 5) {
   let parcels = [];
   for (let i = 0; i < parcelCount; i++) {
     let address = randomPick(Object.keys(roadGraph));
@@ -118,4 +128,55 @@ VillageState.random = function(parcelCount = 5) {
   return new VillageState("Post Office", parcels);
 };
 
-runRobot(VillageState.random(), randomRobot);
+/** A robot who follows the mail route */
+function routeRobot(state, memory) {
+  if (memory.length == 0) {
+    memory = mailRoute;
+  }
+  return { direction: memory[0], memory: memory.slice(1) };
+}
+
+/**
+ * keeps a work list. This is an array of places that should be explored next,
+ * along with the route that got us there. Then operates by taking the next
+ * item in the list and exploring that.
+*/
+
+function findRoute(graph, from, to) {
+  let work = [{ at: from, route: [] }];
+  for (let i = 0; i < work.length; i++) {
+    let { at, route } = work[i];
+    for (let place of graph[at]) {
+      if (place == to) return route.concat(place);
+      if (!work.some(w => w.at == place)) {
+        work.push({ at: place, route: route.concat(place) });
+      }
+    }
+  }
+}
+
+/**
+ * When route is empty, robot moves to either pick up the parcel or
+ * deliver the parcel.
+ */
+function goalOrientedRobot({ place, parcels }, route) {
+  if (route.length == 0) {
+    let parcel = parcels[0];
+    if (parcel.place != place) {
+      route = findRoute(roadGraph, place, parcel.place);
+    } else {
+      route = findRoute(roadGraph, place, parcel.address);
+    }
+  }
+  return { direction: route[0], memory: route.slice(1) };
+}
+runRobot(VillageState.random(), goalOrientedRobot, []);
+
+/************************************************************* Exercises */
+
+/** Robot Efficiency */
+
+/** Persistant Group */
+
+
+
