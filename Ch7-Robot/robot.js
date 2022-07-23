1ea3ba1476e75f2a33fb6e260313e0e8e9fb3701
calebthewood <calebwood.cs@gmail.com>
2022-07-23 09:36:44 -0700

    Completes Random Robot

--- Ch7-Robot/robot.js
+++ Ch7-Robot/robot.js
@@ -74,8 +74,48 @@ let first = new VillageState(
   [{ place: "Post Office", address: "Alice's House" }]
 );
 
-let next = first.move("Alice's House");
+// let next = first.move("Alice's House");
 
-console.log(next.place);
-console.log(next.parcels);
-console.log(first.place);
+// console.log(next.place);
+// console.log(next.parcels);
+// console.log(first.place);
+
+function runRobot(state, robot, memory) {
+  for (let turn = 0; ; turn++) {
+    if (state.parcels.length == 0) {
+      console.log(`Done in ${turn} turns`);
+      break;
+    }
+    let action = robot(state, memory);
+    state = state.move(action.direction);
+    memory = action.memory;
+    console.log(`Moved to ${action.direction}`);
+  }
+}
+
+/** Returns a random choice from an array */
+function randomPick(array) {
+  let choice = Math.floor(Math.random() * array.length);
+  return array[choice];
+}
+
+/** A robot who chooses its next destination at random */
+function randomRobot(state) {
+  return { direction: randomPick(roadGraph[state.place]) };
+}
+
+
+VillageState.random = function(parcelCount = 5) {
+  let parcels = [];
+  for (let i = 0; i < parcelCount; i++) {
+    let address = randomPick(Object.keys(roadGraph));
+    let place;
+    do {
+      place = randomPick(Object.keys(roadGraph));
+    } while (place == address);
+    parcels.push({ place, address });
+  }
+  return new VillageState("Post Office", parcels);
+};
+
+runRobot(VillageState.random(), randomRobot);
