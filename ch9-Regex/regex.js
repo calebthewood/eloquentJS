4501af683f6747df88a19c34267a41fde83eb8cf
calebthewood <calebwood.cs@gmail.com>
2022-07-26 17:03:31 -0700

    Ch8 exercises

--- ch9-Regex/regex.js
+++ ch9-Regex/regex.js
@@ -50,7 +50,40 @@ let cartoonCrying = /boo+(hoo)+/i; // i = case insensitive
 // log(cartoonCrying.test("Boohooohoohooo")); //true
 
 let match = /\d+/.exec("one two 100");
-log(match); // [ '100', index: 8, input: 'one two 100', groups: undefined ]
-log(match.index); // 8, index where the matched pattern begins
+// log(match); // [ '100', index: 8, input: 'one two 100', groups: undefined ]
+// log(match.index); // 8, index where the matched pattern begins
 
-log("one two 100".match(/\d+/));
+// log("one two 100".match(/\d+/));
+
+let quotedText = /'([^']*)'/;
+// log(quotedText.exec("she said 'hello'"));
+
+// log(/bad(ly)?/.exec("bad"));
+// --> [ 'bad', undefined, index: 0, input: 'bad', groups: undefined ]
+// log(/(\d)+/.exec("123"));
+// --> [ '123', '3', index: 0, input: '123', groups: undefined ]
+// 1st element is the whole match
+// 2nd element is the (subexpression) match
+
+/** creates date object from a string */
+function getDate(string) {
+  let [_, month, day, year] =
+    /(\d{1,2})-(\d{1,2})-(\d{4})/.exec(string);
+  return new Date(year, month - 1, day);
+}
+log(getDate("1-30-2003"));
+
+/* Word and String Boundaries */
+// /^\d+$/ --> Matches a string consisting entirely of one or more digits
+// /^!/ --> Mathces any string that starts with an exclamation mark
+// /x^/ --> Does not match any string, cannot have an x before the string starts
+// \b --> \b looks for a word boundary, like a space or symbol
+// \w --> not sure.
+
+log(/cat/.test("concatenate")); //true:
+log(/\bcat\b/.test("concatenate")); //false:
+log(/\bcat\b/.test("con.cat.enate")); //true:
+
+
+
+
