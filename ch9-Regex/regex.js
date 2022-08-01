8f56161b50e971be14c438e95982f683a5a0835e
calebthewood <calebwood.cs@gmail.com>
2022-07-31 19:10:11 -0700

    Add number catcher

--- ch9-Regex/regex.js
+++ ch9-Regex/regex.js
@@ -8,82 +8,94 @@ Regex methods:
 
 const log = console.log;
 
-// regex expressions can be written either way
-let re1 = new RegExp("abc");
-let re2 = /abc/;
+// // regex expressions can be written either way
+// let re1 = new RegExp("abc");
+// let re2 = /abc/;
 
-//in regex, backslashes preserve symbols
-let eighteenPlus = /eighteen\+/;
+// //in regex, backslashes preserve symbols
+// let eighteenPlus = /eighteen\+/;
 
 // log(re1.test("abcde")); //true
 // log(re1.test("axcde")); // false
 
-//Both find the same sets
+// //Both find the same sets
 // log(/[0123456789]/.test("in 1992")); //true
 // log(/[0-9]/.test("in 1992")); //true
 
-// ^ to match any character except the ones in the set
-let notBinary = /[^01]/;
+// // ^ to match any character except the ones in the set
+// let notBinary = /[^01]/;
 // log(notBinary.test("1100100010100110")) // false: all chars are either 0,1
 // log(notBinary.test("1100100010200110")) // true: contains chars other than 0,1
 
-// + to match more than one of a thing.
+// // + to match more than one of a thing.
 // log(/'\d+'/.test("'123'")); //true: /\d+/ matches one or more digits
 // log(/'\d+'/.test("''")); //false: /\d+/ no digits
 // log(/'\d*'/.test("'123'")); //true: /\d*/ similar to + but matches on 0
 // log(/'\d*'/.test("''")); //false: see above
 
-// ? makes pattern optional
-let neighbor = /neighbou?r/;
+// // ? makes pattern optional
+// let neighbor = /neighbou?r/;
 // log(neighbor.test("neighbour")); //true: marks the u as optional
 // log(neighbor.test("neighbor")); //true
 
-/* {} to signify number of times a patter will occur
- {4} = 4x, {1,2} = 1x or 2x, {5,} = 5x or more */
-let dateTime = /\d{1,2}-\d{1,2}-\d{4} \d{1,2}:\d{2}/;
-// true: days and months have 1-2 digits, years have 4 digits, etc.
+// /* {} to signify number of times a patter will occur
+//  {4} = 4x, {1,2} = 1x or 2x, {5,} = 5x or more */
+// let dateTime = /\d{1,2}-\d{1,2}-\d{4} \d{1,2}:\d{2}/;
+// //  true: days and months have 1-2 digits, years have 4 digits, etc.
 // log(dateTime.test("1-30-2003 8:45"));
 
-// () groups subexpressions
-let cartoonCrying = /boo+(hoo)+/i; // i = case insensitive
-//regex looking for "bo" and "ho" either with one or more o's
+// // () groups subexpressions
+// let cartoonCrying = /boo+(hoo)+/i; // i = case insensitive
+// // regex looking for "bo" and "ho" either with one or more o's
 // log(cartoonCrying.test("Boohooohoohooo")); //true
 
-let match = /\d+/.exec("one two 100");
+// let match = /\d+/.exec("one two 100");
 // log(match); // [ '100', index: 8, input: 'one two 100', groups: undefined ]
 // log(match.index); // 8, index where the matched pattern begins
 
 // log("one two 100".match(/\d+/));
 
-let quotedText = /'([^']*)'/;
-// log(quotedText.exec("she said 'hello'"));
+// let quotedText = /'([^']*)'/;
+// // log(quotedText.exec("she said 'hello'"));
 
 // log(/bad(ly)?/.exec("bad"));
-// --> [ 'bad', undefined, index: 0, input: 'bad', groups: undefined ]
+// // --> [ 'bad', undefined, index: 0, input: 'bad', groups: undefined ]
 // log(/(\d)+/.exec("123"));
-// --> [ '123', '3', index: 0, input: '123', groups: undefined ]
-// 1st element is the whole match
-// 2nd element is the (subexpression) match
+// // --> [ '123', '3', index: 0, input: '123', groups: undefined ]
+// // 1st element is the whole match
+// // 2nd element is the (subexpression) match
 
-/** creates date object from a string */
-function getDate(string) {
-  let [_, month, day, year] =
-    /(\d{1,2})-(\d{1,2})-(\d{4})/.exec(string);
-  return new Date(year, month - 1, day);
-}
-log(getDate("1-30-2003"));
-
-/* Word and String Boundaries */
-// /^\d+$/ --> Matches a string consisting entirely of one or more digits
-// /^!/ --> Mathces any string that starts with an exclamation mark
-// /x^/ --> Does not match any string, cannot have an x before the string starts
-// \b --> \b looks for a word boundary, like a space or symbol
-// \w --> not sure.
-
-log(/cat/.test("concatenate")); //true:
-log(/\bcat\b/.test("concatenate")); //false:
-log(/\bcat\b/.test("con.cat.enate")); //true:
+// /** Creates date object from a string
+//  *
+//  * ex: "1-30-2003" --> 2003-01-30T08:00:00.000Z
+// */
+// function getDate(string) {
+//   let [_, month, day, year] =
+//     /(\d{1,2})-(\d{1,2})-(\d{4})/.exec(string);
+//   return new Date(year, month - 1, day);
+// }
 
 
+// log(getDate("1-30-2003"));
 
+// /* Word and String Boundaries */
+// // /^\d+$/ --> Matches a string consisting entirely of one or more digits
+// // /^!/ --> Mathces any string that starts with an exclamation mark
+// // /x^/ --> Does not match any string, cannot have an x before the string starts
+// // \b --> \b looks for a word boundary, like a space or symbol
+// // \w --> not sure.
+
+// log(/cat/.test("concatenate")); //true:
+// log(/\bcat\b/.test("concatenate")); //false:
+// log(/\bcat\b/.test("con.cat.enate")); //true:
+
+let animalCount = /\b\d+ (pig|cow|chicken)s?\b/;
+// find a digit followed by either pig(s), cow(s), OR chicken(s)
+log(animalCount.test("15 pigs")); //true
+log(animalCount.test("15 pigchickens")); //false
+
+let binaryOrHex = /\b([01]+b|[\da-f]+h|\d+)\b/;
+/* matches either a binary number followed by a b,
+a hexadecimal number followed by an h, or a decimal with no suffix
+*/
 
--- practice.js
+++ practice.js
@@ -21,3 +21,76 @@ function maxSubArray(numbers) {
   }
   return maxSum
 }
+
+https://leetcode.com/problems/merge-two-sorted-lists/
+
+
+/*
+2d map of islands and water
+1 = land
+0 = water
+only horizontal and vertical connections count
+out of bounds is water
+ [[],[],[]] --> island count (Int)
+
+ Input: grid = [
+  ["1","1","1","1","0"],
+  ["1","1","0","1","0"],
+  ["1","1","0","0","0"],
+  ["0","0","0","0","0"]
+]
+Output: 1
+
+Input: grid = [
+  ["1","1","0","0","0"],
+  ["1","1","0","0","0"],
+  ["0","0","1","0","0"],
+  ["0","0","0","1","1"]
+]
+Output: 3
+
+//outter fn
+// islandCount
+// visited = []
+// loop over arrays, call inner fn at each element
+  // if 0, or in visited, skip
+  // inner fn
+  //base case: all nswe are water/undefined
+    // islandCount += 1
+  // add any adjacent "land" nodes to queue
+  // recurse on 1st element in our queue
+*/
+
+function islandCounter(arrays) {
+  let islandCount = 0;
+  const visited = [];
+  const lenY = arrays.length;
+  const lenX = arrays[0].length;
+
+  function landCounter(y, x) {
+    let coords = `${yx}`;
+    if (visited.includes(coords)) return false;
+    if (arrays[y][x] === "0") return false;
+    visited.push(coords);
+
+    let N = arrays[y - 1][x];
+    let S = arrays[y + 1][x];
+    let E = arrays[y][x + 1];
+    let W = arrays[y][x - 1];
+    // coord is "1"
+    if (N) landCounter(y - 1, x);
+    if (S) landCounter(y + 1, x);
+    if (E) landCounter(y, x + 1);
+    if (W) landCounter(y, x - 1);
+    return true;
+  }
+
+  for (let y = 0; y < lenY; y++) {
+    for (let x = 0; x < lenX; x++) {
+      if (landCounter(y, x)) {
+        islandCount++;
+      }
+    }
+  }
+  return islandCount;
+}
