196b3b20c8104132275c5907de64e6107be1dfbe
calebthewood <calebwood.cs@gmail.com>
2022-08-20 10:45:02 -0700

    Add replaceImage

--- ch14-DOM/index.js
+++ ch14-DOM/index.js
@@ -16,3 +16,15 @@ function talksAbout(node, string) {
 }
 
 console.log(talksAbout(document.body, "text to look for"))
+
+/** Replaces image tags with the text from their "alt" attribute */
+function replaceImages() {
+  let images = document.body.getElementsByTagName('img');
+  for (let i = images.length -1; i >=0; i--) {
+    let image = images[i];
+    if (image.alt) {
+      let text = document.createTextNode(image.alt);
+      image.parentNode.replaceChild(text, image)
+    }
+  }
+}
