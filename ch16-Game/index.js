25d54bdda8f43dddb95b7ee247969d7f433416f4
calebthewood <calebwood.cs@gmail.com>
2022-08-27 09:31:56 -0700

    Add DomSyncState

--- ch16-Game/index.js
+++ ch16-Game/index.js
@@ -167,6 +167,16 @@ function drawActors(actors) {
     rect.style.left = `${actor.pos.x * scale}px`;
     rect.style.top = `${actor.pos.y * scale}px`;
     return rect;
-  }))
+  }));
 }
+
+DOMDisplay.prototype.syncState = function(state) {
+  if (this.actorLayer) this.actorLayer.remove();
+  this.actorLayer = drawActors(state.actors);
+  this.dom.appendChild(this.actorLayer);
+  this.dom.className = `game ${state.status}`;
+  this.scrollPlayerIntoView(state);
+};
+
+
 
