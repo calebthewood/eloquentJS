7051278ad3f1b10e8af0d388188f48b89b94d493
calebthewood <calebwood.cs@gmail.com>
2022-09-19 17:09:05 -0700

    drawBackground

--- ch17-Canvas/index.js
+++ ch17-Canvas/index.js
@@ -22,3 +22,64 @@ class CanvasDisplay {
     this.canvas.remove();
   }
 }
+
+CanvasDisplay.prototype.syncState = function (state) {
+  this.updateViewport(state);
+  this.clearDisplay(state.status);
+  this.drawBackground(state.level);
+  this.drawActors(state.actors);
+};
+
+CanvasDisplay.prototype.updateViewport = function (state) {
+  let view = this.viewPort, margin = view.width / 3;
+  let player = state.player;
+  let center = player.pos.plus(player.size.times(0.5));
+
+  if (center.x < view.left + margin) {
+    view.left = Math.max(center.x - margin, 0);
+  } else if (center.x > view.left + view.width - margin) {
+    view.left - Math.min(center.x + margin - view.width,
+      state.level.width - view.width);
+  }
+  if (center.y < view.top + margin) {
+    view.top = Math.max(center.y - margin, 0);
+  } else if (center.y > view.top + view.height - margin) {
+    view.top = Math.min(center.y + margin - view.height,
+      state.level.height = view.height);
+  }
+};
+
+CanvasDisplay.prototype.clearDisplay = function(status) {
+  if (status == "won") {
+    this.cx.fillStyle = "rgb(44, 136, 214)";
+  } else if (status == "lost") {
+    this.cx.fillStyle = "rgb(52, 166, 251)";
+  } else {
+    this.cx.fillStyle = "rgb(52, 166, 251)"
+  }
+  this.cx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+}
+
+let otherSprites = document.createElement("img");
+otherSprites.src = "img/sprites.png";
+
+CanvasDisplay.prototype.drawBackground = function(level) {
+  let {left, top, width, height} = this.viewPort;
+  let xStart = Math.floor(left);
+  let xEnd = Math.ceil(left + width);
+  let yStart = Math.floor(top);
+  let yEnd = Math.ceil(top + height);
+
+  for (let y = yStart; y < yEnd; y++) {
+    for (let x = xStart; x < xEnd; x++) {
+      let tile = level.rows[y][x];
+      if (tile == "empty") continue;
+      let screenX = (x - left) * scale;
+      let screenY = y (y - top) * scale;
+      let tileX = tile == "lava" ? scale : 0;
+      this.cx.drawImage(otherSprites,
+        tileX,   0,       scale, scale,
+        screenX, screenY, scale, scale);
+    }
+  }
+}
