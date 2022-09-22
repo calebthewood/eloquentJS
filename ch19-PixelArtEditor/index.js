0ef1ce642f18ac4ca3b19344d8a939bd722c20a7
calebthewood <calebwood.cs@gmail.com>
2022-09-22 15:01:35 -0700

    add index.js

--- ch19-PixelArtEditor/index.js
+++ ch19-PixelArtEditor/index.js
@@ -1,5 +1,21 @@
 "use strict";
 
+/* ******************************************* Utility Functions */
+function updateState(state, action) {
+  return { ...state, ...action };
+}
+
+function elt(type, props, ...children) {
+  let dom = document.createElement(type);
+  if (props) Object.assign(dom, props); // try dom = {dom, ...props}
+  for (let child of children) {
+    if (typeof child != "string") dom.appendChild(child);
+    else dom.appendChild(document.createTextNode(child));
+  }
+  return dom;
+}
+
+/* **************************************** End Utility Functions */
 class Picture {
   constructor(width, height, pixels) {
     this.width = width;
@@ -18,9 +34,16 @@ class Picture {
 
   draw(pixels) {
     let copy = this.pixels.slice();
-    for (let {x, y, color} of pixels) {
+    for (let { x, y, color } of pixels) {
       copy[x + y * this.width] = color;
     }
-    return new Picture(this.width, this.height, copy)
+    return new Picture(this.width, this.height, copy);
   }
 }
+
+
+/* *************************************** Event Listeners */
+
+document.body.appendChild(elt("button", {
+  onClick: () => console.log("click")
+}, "The button"));
