c5541d77f4436c91af1666a736068ad2a0debe7a
calebthewood <calebwood.cs@gmail.com>
2022-09-22 17:10:44 -0700

    Finish LoadButton

--- ch19-PixelArtEditor/LoadButton.js
+++ ch19-PixelArtEditor/LoadButton.js
@@ -1,12 +1,12 @@
 "use strict";
 
 class LoadButton {
-  constructor(_, {dispatch}) {
+  constructor(_, { dispatch }) {
     this.dom = elt("button", {
       onclick: () => startLoad(dispatch)
     }, "📁 Load");
   }
-  syncState() {}
+  syncState() { }
 }
 
 function startLoad(dispatch) {
@@ -17,4 +17,37 @@ function startLoad(dispatch) {
   document.body.appendChild(input);
   input.click();
   input.remove();
+}
+
+function finishLoad(file, dispatch) {
+  if (file == null) return;
+  let reader = new FileReader();
+  reader.addEventListener("load", () => {
+    let image = elt("img", {
+      onload: () => dispatch({
+        picture: pictureFromImage(image)
+      }),
+      src: reader.result
+    });
+  });
+  reader.readAsDataURL(file);
+}
+
+function pictureFromImage(image) {
+  let width = Math.min(100, image.width);
+  let height = Math.min(100, image.height);
+  let canvas = elt("canvas", {width, height});
+  cx = canvas.getContext("2d")
+  cx.drawImage(image, 0, 0);
+  let pixels = [];
+  let {data} = cx.getImageData(0, 0, width, height);
+
+  function hex(n) {
+    return n.toString(16).padStart(2, "0");
+  }
+  for (let i = 0; i < data.length; i += 4) {
+    let [r, g, b] = data.slice(i, i + 3);
+    pixels.push("#" + hex(r) + hex(g) + hex(b));
+  }
+  return new Picture(width, height, pixels);
 }
